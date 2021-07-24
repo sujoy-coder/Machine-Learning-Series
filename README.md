4d230e184c7eea8b66f071cba3f4b52f91e21d70
sujoy-coder <ask2sujoy@gmail.com>
2021-07-25 00:27:23 +0530

    modified liner_model.py

--- README.md
+++ README.md
@@ -13,4 +13,57 @@ How to import this Model
 from regression.linear_model import SimpleRegression
 `
 
+- ## Linear Regression / Multiple Regression 
+<br>
+Theoretical Part
+
+##### x is independent variables
+##### y is dependent variables
+##### m is number of samples
+##### n is number of features
+
+`x = [[1,2,3,5],
+     [5,6,4,7],
+     [5,6,1,9],
+     [9,7,8,1],
+     [2,4,6,8]]     
+ ||    dimension -> mxn (5x4)`
+<br>
+`y = [7,9,8,6]
+ ||    dimension -> 1xm (1x5)`
+
+## Steps to Solve :
+
+`Equation : (y_predicted) -> z = w1x1 + w2x2 + w3x3 + w4x4 + b`
+
+1. create constructer of class with parameter:
+    weights (dim -> 1xn), bias, learning rate(alpha), epochs
+
+2. forword Propagation:
+    first make x = Transpose(x) -> dim : nxm
+    find, z = wx + b  -> dim : 1xm
+
+3. calculate the cost function:
+    find, J = (1/2*m)sum((z-y)**2)
+
+4. minimize the cost function with back propagation :
+    dw = dJ/dw = dJ/dz * dz/dw
+    db = dJ/db = dJ/dz * dz/db
+    Now, dJ/dz = dz = (1/m)[(z-y)] and dz/dw = x [since z = wx + b]
+    Now, dz/db = 1 [since z = wx + b]
+    So, dw = dz * (dz/dw) = dz * x.T [to avoid dimenssion issue we have to transpose of x]
+    So, db = sum(dz)
+
+5. By gradient decent:
+    w = w - alpha * dw
+    b = b - alpha * db
+
+6. Repect the process [2-5] for epochs number of times
+
+How to import this Model
+
+`
+from regression.linear_model import LinearRegression
+`
+
 #### Next Part Commming Soon ...
--- regression/linear_model.py
+++ regression/linear_model.py
@@ -1,15 +1,15 @@
 import numpy as np
 import math
 
+np.seterr(all='ignore')
+
 class SimpleRegression:
 	''' Formula for Simple Regression is : y = mx + c '''
-
 	def __init__(self):
 		# 'intercept_' is c in the equation of : y = mx + c
 	 	self.intercept_ = None
 	 	# 'coef_' is m in the equation of : y = mx + c
 	 	self.coef_ = None
-
 
 	def __prodSum(self, x, y):
 		''' It is a private method '''
@@ -21,7 +21,6 @@ class SimpleRegression:
 		except Exception as e:
 			print(e)
 
-
 	def __diffSqureSum(self, x1, x2):
 		''' It is a private method '''
 		try:
@@ -31,7 +30,6 @@ class SimpleRegression:
 			return ans
 		except Exception as e:
 			print(e)
-
 
 	def fit(self, x= None, y= None):
 		''' x is feature-dataset / feature column : size --> n x 1
@@ -57,7 +55,6 @@ class SimpleRegression:
 		except Exception as e:
 			print(e)
 
-
 	def predict(self, x_test= None):
 		''' this method takes feature inputs in 1D array/list format
 			and returns predicted output values
@@ -78,7 +75,6 @@ class SimpleRegression:
 		else:
 			print('First Train Your model by using \'fit()\' method...')
 			return None
-
 
 	def scores(self, actual_value= None, predicted_value= None, error_type= 'mse'):
 		''' actual_value => format list/array,
@@ -119,7 +115,6 @@ class SimpleRegression:
 			print(e)
 			return None
 
-
 	def train_test_split(self, x= None, y= None, test_size= 0.3):
 		''' returns --> x_train, y_train, x_test, y_test '''
 		try:
@@ -133,3 +128,111 @@ class SimpleRegression:
 			print(e)
 			return None,None,None,None
 		
+
+class LinearRegression:
+    ''' NOTE : For Large dataset Make sure data must be scaled before training 
+        to get Good Score of the model'''
+    def __init__(self):
+        self.__epochs = 2000
+        self.__lr = 0.074
+
+    def _forword_propagation(self):
+        _z = np.dot(self.__thetas,self.__x)
+        return _z
+    
+    def _compute_cost(self, _z):
+        _cost = (1/2*self.__sample_size) * np.sum(np.square(_z - self.__y))
+        return _cost
+
+    def _back_propagation(self, _z):
+        ''' _d_theta = dJ/dTheta '''
+        _dz = (1/self.__sample_size) * (_z - self.__y)
+        _d_theta = np.dot(_dz,self.__x.T)
+        return _d_theta
+    
+    def _gradient_decent_update(self, _d_theta):
+        self.__thetas = self.__thetas - (self.__lr * _d_theta)
+
+    def _apply(self):
+        self.__gradient_decent_data = {'cost':[],'epochs':[]}
+        for _epoch in range(1,self.__epochs+1):
+            y_hat = self._forword_propagation()
+            _cost_value = self._compute_cost(y_hat)
+            _dt = self._back_propagation(y_hat)
+            self._gradient_decent_update(_dt)
+            if (_epoch == 1) or (_epoch % 20 == 0):
+                if self.__pgd:
+                    self.__gradient_decent_data['cost'].append(_cost_value)
+                    self.__gradient_decent_data['epochs'].append(_epoch)
+                if self.__display_logs:
+                    _mse = np.mean(np.square(y_hat-self.__y))
+                    print(f'Cost Value : {_cost_value}  |  Mean Squred Error : {_mse}  |  Epochs : {_epoch}/{self.__epochs}')
+					
+    def fit(self, x, y, lr= None, epochs= None, display_logs= False, pgd= False):
+        try:
+            self.__pgd = pgd
+            self.__display_logs = display_logs
+            self.__x = np.array(x)
+            self.__sample_size = self.__x.shape[0]
+            _ones = np.ones(self.__sample_size)
+            self.__x = np.c_[_ones,self.__x]
+            self.__feature_size = self.__x.shape[1]
+            self.__x = self.__x.T
+            # self.__thetas = np.zeros((1,self.__feature_size))
+            self.__thetas = np.random.randn(self.__feature_size).reshape(1,self.__feature_size)
+            self.__y = np.array(y).reshape(1,self.__sample_size)
+            if lr:
+                self.__lr = lr
+            if epochs:
+                self.__epochs = epochs
+            self._apply()
+        except Exception as e:
+            raise e
+
+    @property
+    def intercept_(self):
+        try:
+            return self.__thetas[0][0]
+        except:
+            return None
+
+    @property
+    def coef_(self):
+        try:
+            return self.__thetas[0][1:]
+        except:
+            return None
+
+    def predict(self, x):
+        try:
+            _x = np.array(x)
+            _size = _x.shape[0]
+            _ones = np.ones(_size)
+            _x = np.c_[_ones,_x]
+            _y_pred = np.dot(self.__thetas,_x.T)
+            return _y_pred.flatten()
+        except Exception as e:
+            print(e)
+
+    def score(self, y_true, y_pred, error_type='mse'):
+        try:
+            error_list = ['mse','rmse','r2_score']
+            if error_type in error_list:
+                _y_true = np.array(y_true)
+                _y_pred = np.array(y_pred)
+                if error_type == 'r2_score':
+                    _error = 1 - ((np.sum((_y_true-_y_pred)**2)) / (np.sum((_y_true-np.mean(_y_true))**2)))
+                    return _error
+                elif error_type == 'rmse':
+                    _error = (1/len(_y_true)) * np.sum((_y_true-_y_pred)**2)
+                    return np.sqrt(_error)
+                elif error_type == 'mse':
+                    _error = (1/len(_y_true)) * np.sum((_y_true-_y_pred)**2)
+                    return _error
+                else:
+                    pass
+            else:
+                print(f'Error Invalid Matrix Name... Please choose among : {error_list}')
+        except Exception as e:
+            print(e)
+
